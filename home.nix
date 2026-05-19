9315431f8240e950f1d2039d6a2112d87f8be330
RionaldiDwipurna <70762968+RionaldiDwipurna@users.noreply.github.com>
2026-05-19 16:45:20 +0700

    add hyprland config

--- home.nix
+++ home.nix
@@ -1,5 +1,14 @@
 {config, pkgs, ...}:
+let
+  dotfiles = "${config.home.homeDirectory}/dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
 
+  configs = {
+    hypr = "hypr";
+    nvim = "nvim";
+    waybar = "waybar";
+  };
+in
 {
   home.username = "rdwp";
   home.homeDirectory = "/home/rdwp";
@@ -24,18 +33,25 @@
       nrs = "sudo nixos-rebuild switch --flake ~/dotfiles#rdwp";
     };
 
-    profileExtra = ''
-      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        if uwsm check may-start;then
-          exec uwsm start hyprland-uwsm.desktop
-        fi 
-      fi
-    '';
+    #profileExtra = ''
+    #  if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+    #    if uwsm check may-start;then
+    #      exec uwsm start hyprland-uwsm.desktop
+    #    fi 
+    #  fi
+    #'';
   };
+
   programs.fastfetch = {
     enable = true;
   };
 
-
-
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      # source = config.lib.file.mkOutOfStoreSymlink
+      #   "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
 }
